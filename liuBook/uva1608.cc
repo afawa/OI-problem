500c49a3c3e472894daff7abb7db61a06dbcd97e
afawa <1700012786@pku.edu.cn>
2020-09-27 07:44:25 +0800

    9/26

--- liuBook/uva1608.cc
+++ liuBook/uva1608.cc
@@ -1,59 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,a[200005];
-map<int,int> mps[20][200005];
-void init(){
-    for(int i=0;i<20;++i){
-        for(int j=0;j<200005;++j)
-            mps[i][j].clear();
-    }
-}
-
-bool flag;
-bool merge(int left,int right,int level){
-    if(flag == false)
-        return false;
-    if(left == right){
-        mps[level][left][a[left]] = 1;
+int n,a[200005],leftFirst[200005],rightFirst[200005];
+unordered_map<int,int> mp;
+bool check(int left,int right){
+    if(left >= right)
         return true;
-    }
-    int mid = (left + right) / 2;
-    mps[level][left] = mps[level+1][left];
-    
-    for(int i=mid+1;i<=right;++i){
-        if(mps[level+1][left].count(a[i]))
-            mps[level][left][a[i]]++;
-        else{
-            mps[level][left][a[i]] = 1;
+    int leftPoint = left,rightPoint = right;
+    while(rightPoint >= leftPoint){
+        if(leftFirst[rightPoint]<left && rightFirst[rightPoint]>right){
+            if(check(left,rightPoint-1) && check(rightPoint+1,right))
+                return true;
         }
-    }
-    for(auto it= mps[level][left].begin();it!=mps[level][left].end();++it){
-        if(it->second == 1){
-            return true;
+        rightPoint--;
+        if(leftFirst[leftPoint]<left && rightFirst[leftPoint]>right){
+            if(check(left,leftPoint-1) && check(leftPoint+1,right))
+                return true;
         }
+        leftPoint++;
     }
-    flag = false;
-    return false;
-}
-
-bool split(int left,int right,int level){
-    if(flag == false)
-        return false;
-    int mid = (left + right) / 2;
-    bool ans = split(left,mid,level+1) & split(mid+1,right,level+1);
-    ans &= merge(left,right,level);
-    return ans;
+    return 0;
 }
 int main(){
     int t;
     scanf("%d",&t);
     while(t--){
         scanf("%d",&n);
-        flag = true;
         for(int i=0;i<n;++i){
             scanf("%d",a+i);
+            leftFirst[i] = -1;
+            rightFirst[i] = n;
         }
-        puts(split(0,n-1,0)?"non-boring" : "boring");
+        mp.clear();
+        for(int i=0;i<n;++i){
+            if(mp.count(a[i])){
+                leftFirst[i] = mp[a[i]];
+                rightFirst[mp[a[i]]] = i;
+            }
+            mp[a[i]] = i;
+        }
+        puts(check(0,n-1) ? "non-boring" : "boring");
     }
     return 0;
 }
